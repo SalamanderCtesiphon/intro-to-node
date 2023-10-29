77269393ae7427c348c1409b80aa043f37232b08
salamanderctesiphon <swbrookshire@gmail.com>
2023-10-29 15:24:59 -0500

    created my first node module and included it in my server

--- myFirst.js
+++ myFirst.js
@@ -1,8 +1,10 @@
 const http = require('http')
+const dt = require('./myfirstmodule')
 
 http
   .createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/html' })
-    res.end('Hello World')
+    res.write('The date and time are currntly: ' + dt.myDateTime())
+    res.end()
   })
   .listen(8080)
