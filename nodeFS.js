852ae3a7b77f0a561bb980b5987dcf8c4dd90032
salamanderctesiphon <swbrookshire@gmail.com>
2023-10-29 14:55:35 -0500

    used the node fs module method to read from a file

--- nodeFS.js
+++ nodeFS.js
@@ -1,7 +1,28 @@
 const fs = require('fs')
 
-fs.rename('before.json', 'after.json', (err) => {
+// rename a file
+/* fs.rename('before.json', 'after.json', (err) => {
   if (err) {
     return console.log(err)
   }
+})
+ */
+
+// Add to a file
+/* const content = 'Some Content'
+
+fs.writeFile('test.txt', content, (err) => {
+  if (err) {
+    console.log(err)
+  }
+}) */
+
+// Read a file
+
+fs.readFile('test.txt', 'utf-8', (err, data) => {
+  if (err) {
+    console.error(err)
+    return
+  }
+  console.log(data)
 })
