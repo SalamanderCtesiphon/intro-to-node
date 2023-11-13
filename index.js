8ea74fc565b14a8a62ff8255e29cb959f7504b4f
salamanderctesiphon <swbrookshire@gmail.com>
2023-11-13 14:22:03 -0600

    added a rout to 404 on the server script

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ http
     fs.readFile(filename, function (err, data) {
       if (err) {
         res.writeHead(404, { 'Content-Type': 'text/html' })
-        return res.end('404 Not Found')
+        return (filename = './404.html')
       }
       res.writeHead(200, { 'Content-Type': 'text/html' })
       res.write(data)
